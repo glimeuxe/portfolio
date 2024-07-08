0c4be52e3319112c95b047d458627e565b19775b
Gregory <49094805+glimeuxe@users.noreply.github.com>
2024-07-08 17:33:57 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-const swiper = new Swiper("swiper-container");
-
 function checkViewportWidth() {
-	var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-	var wrappers = document.querySelectorAll("wrapper");
-	var viewportAlerts = document.querySelectorAll("viewport-alert");
+	let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+	let wrappers = document.querySelectorAll("wrapper");
+	let viewportAlerts = document.querySelectorAll("viewport-alert");
 	if (viewportWidth < 360) {
-		wrappers.forEach((wrapper) => {
-			wrapper.style.display = "none";
-		});
-		viewportAlerts.forEach((viewportAlert) => {
-			viewportAlert.style.display = "flex";
-		});
+		wrappers.forEach((wrapper) => {wrapper.style.display = "none";});
+		viewportAlerts.forEach((viewportAlert) => {viewportAlert.style.display = "flex";});
 	} else {
-		wrappers.forEach((wrapper) => {
-			wrapper.style.display = "";
-		});
-		viewportAlerts.forEach((viewportAlert) => {
-			viewportAlert.style.display = "none";
-		});
+		wrappers.forEach((wrapper) => {wrapper.style.display = "";});
+		viewportAlerts.forEach((viewportAlert) => {viewportAlert.style.display = "none";});
 	}
 }
 
 checkViewportWidth();
-window.addEventListener("resize", function () {
-	checkViewportWidth();
-});
+window.addEventListener("resize", checkViewportWidth);
